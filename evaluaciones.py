98342ab8145f145188a7a9f96399190c1a6d9c94
David Garcia <gtdavid2013@gmail.com>
2023-01-01 23:48:13 -0600

    sql alchemy placeholders

--- evaluaciones.py
+++ evaluaciones.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from forms import CommentForm
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db' #path to database and its name
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warning
+#db = SQLAlchemy(app) #database instance
+
 
 @app.route('/')
 def index():
@@ -17,5 +21,5 @@ def client(id):
       pass
       # new_comment = comment_form.comment.data
       # comments[id].append(new_comment)
-      # return redirect(url_for("almacen", id=new_id, _external=True, _scheme="https"))
+      # return redirect(url_for("almacen", id=new_id, _external=True, _scheme="https")) # for redirection those args are needed
   #return render_template('cliente.html')
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField, RadioField
 from wtforms.validators import DataRequired
 
 class CommentForm(FlaskForm):
@@ -7,3 +7,13 @@ class CommentForm(FlaskForm):
   # my_radio_group = RadioField("Radio Group Label", choices=[("id1", "One"), ("id2","Two"), ("id3","Three")])
   comment =  StringField("Comment", validators=[DataRequired()])
   submit = SubmitField("Agregar Comentario")
+
+class FieldsRequiredForm(FlaskForm):
+  """Require radio fields to have content. This works around the bug that WTForms radio fields don't honor the `DataRequired` or `InputRequired` validators."""
+  class Meta:
+    def render_field(self, field, render_kw):
+      if field.type == "_Option":
+        render_kw.setdefault("required", True)
+      return super().render_field(field, render_kw)
+
+categories = [("recommended","Recommended"), ("tovisit", "Places To Go"), ("visited", "Visited!!!")]
