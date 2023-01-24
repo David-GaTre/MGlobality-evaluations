ec1cd347051f0ed8e797532142630b998f8bf1b3
David Garcia <gtdavid2013@gmail.com>
2023-01-23 23:10:19 -0600

    Corrected general structure

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -7,55 +7,17 @@ app.config["SECRET_KEY"] = "mysecret"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myDB.db' #path to database and its name
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress warning
 db = SQLAlchemy(app) #database instance
+# To create everything is with db.create_all()
 migrate = Migrate(app, db)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128))
+@app.route('/')
+@app.route('/index')
+def greeting():
+    return render_template('greeting.html')
 
-#declaring the Book model
-#class Book(db.Model):
-#    id = db.Column(db.Integer, primary_key = True) #primary key column
-#    title = db.Column(db.String(80), index = True, unique = True) # book title
-#    author_name = db.Column(db.String(50), index = True, unique = False)
-#    author_surname = db.Column(db.String(80), index = True, unique = False) #author surname
-#    month = db.Column(db.String(20), index = True, unique = False) #the month of the book suggestion
-#    year = db.Column(db.Integer, index = True, unique = False) #the year of the book suggestion
-#    reviews = db.relationship('Review', backref = 'book', lazy = 'dynamic') #relationship of Books and Reviews
-    
-    #Get a nice printout for Book objects
-#    def __repr__(self):
-#        return "{} in: {},{}".format(self.title, self.month, self.year)
+# app name 
+@app.errorhandler(404) 
+def not_found(e): 
+  return render_template("404.html") 
 
-#Add your columns for the Reader model here below.
-#class Reader(db.Model):
-#    id = db.Column(db.Integer, primary_key = True)
-#    name = db.Column(db.String(50), index = True, unique = False)
-#    surname = db.Column(db.String(80), unique = False, index = True)
-##    email = db.Column(db.String(120), unique = True, index = True)
- #   reviews = db.relationship('Review', backref='reviewer', lazy = 'dynamic')
-  
-    #get a nice printout for Reader objects
-#    def __repr__(self):
-#        return "Reader: {}".format(self.email)
-
-#declaring the Review model
-#class Review(db.Model):
-#    id = db.Column(db.Integer, primary_key = True) #primary key column, automatically generated IDs
-#    stars = db.Column(db.Integer, unique = False) #a review's rating
-#    text = db.Column(db.String(200), unique = False) #a review's text
-    #here below is the foreign key column linking to the primary key (id) of the Book model (book). 
-    #Note the lower case here: 'book.id' instead of 'Book.id'
-#    book_id = db.Column(db.Integer, db.ForeignKey('book.id')) #foreign key column
-    #your code here
-#    reviewer_id = db.Column(db.Integer, db.ForeignKey('reader.id'), cascade = 'all, delete, delete-orphan')
-    #get a nice printout for Review objects
-#    def __repr__(self):
-#        return "Review: {} stars: {}".format(self.text, self.stars)
-
-# add datas
-#db.session.add(new_reader1)
-#try:
-#    db.session.commit()
-#except:
-#    db.session.rollback()
+import routes
--- routes.py
+++ routes.py
@@ -1,10 +1,11 @@
-from flask import render_template, request, redirect, url_for
+from flask import Flask
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from forms import CommentForm
-from app import app, db # also enlist models needed
-
-@app.route('/')
-def index():
-  return render_template('index.html')
+from app import app, db
+from models import *  # Only enlist the used models
+from flask import render_template, request, url_for, redirect, flash
 
 @app.route("/cliente/<int:id>")
 def client(id):
@@ -18,3 +19,59 @@ def client(id):
     #review = Review.query.filter_by(id=review_id).first_or_404(description='xd')
 
   #return render_template('cliente.html')
+
+#@app.route('/profiles')
+#def profiles():
+#    current_users = User.query.all() #change here to a database query
+#    return render_template('users.html', current_users = current_users)
+
+#@app.route('/profile/<int:user_id>')
+#def profile(user_id):
+#   user = User.query.filter_by(id = user_id).first_or_404(description = "No such user found.")
+#   songs = Song.query.all()
+#   my_playlist = None #change here to a database query
+#   return render_template('profile.html', user = user, songs = songs, my_playlist = my_playlist)
+
+#Adds new songs to a user's playlist from the song library
+#redirects back to the profile that issued the addition
+#app.route('/add_item/<int:user_id>/<int:song_id>/<int:playlist_id>')
+#def add_item(user_id, song_id, playlist_id):
+#   new_item = Item(song_id = song_id, playlist_id = playlist_id)
+#   user = User.query.filter_by(id = user_id).first_or_404(description = "No such user found.")
+#   my_playlist = Playlist.query.filter_by(id = user.playlist_id).first()
+#   if not exists(new_item, my_playlist.items):
+#      song = Song.query.get(song_id)
+#      #using db session add the new item
+#      db.session.add(new_item)
+#      #increase the counter for the song associated with the new item
+#      song.n = song.n + 1
+#      #commit the database changes here
+#      try:
+#        db.session.commit()
+#      except:
+#        db.session.rollback()
+#   return redirect(url_for('profile', user_id = user_id))
+
+
+#@app.route('/remove_item/<int:user_id>/<int:item_id>')
+#def remove_item(user_id, item_id):
+#   #from the Item model, fetch the item with primary key item_id to be deleted
+#   item = Item.query.get(item_id)
+#   #using db.session delete the item
+#   db.session.delete(item)
+#   #commit the deletion
+#   db.session.commit()
+#   return redirect(url_for('profile', user_id = user_id))
+
+#@app.route('/dashboard', methods=["GET", "POST"])
+#def dashboard():
+#  form = SongForm()
+#  if request.method == 'POST' and form.validate():
+#    new_song = Song(title=form.title, artist=form.artist, n=1)
+#    db.session.add(new_song)
+#    db.session.commit()
+#  else:
+#        flash(form.errors)
+#  unpopular_songs = Song.query.order_by(Song.n)  #add the ordering query here
+#  songs = Song.query.all()
+#  return render_template('dashboard.html', songs = songs, unpopular_songs = unpopular_songs, form = form)
